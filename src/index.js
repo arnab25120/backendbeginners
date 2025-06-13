e5e589905e05db051ddba831c8533696ba81ebd0
Arnab Chakraborty <arnabx36@gmail.com>
2025-06-13 14:06:31 +0530

    Connect to MongoAtlas

---
+++ src/index.js
@@ -0,0 +1,50 @@
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1.
+//Connect to backend using IIFE
+/* import express from "express"
+const app=express()
+(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("error",()=>{
+                console.log("ERR:",error);
+                throw error
+            });
+            app.listen(process.env.PORT,()=>{
+                console.log(`App is listening on port ${process.env.PORT}`);
+                
+            })
+        })
+    } catch(error){
+        console.log("ERROR:",error);
+        throw error
+        
+    }
+})() */
