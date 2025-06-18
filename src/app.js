be5a39f813c4be81fa4e5425eca7de36dfe1960b
Arnab Chakraborty <arnabx36@gmail.com>
2025-06-18 11:38:21 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,9 +10,16 @@ app.use(cors({
     credentials:true
 }))
 
-app.use(expres.json({limit:"16kb"}))
+app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+// http://locahost:8000/api/v1/users/register
 
 export {app}
